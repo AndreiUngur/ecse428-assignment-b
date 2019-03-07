7b1496b87ad5cd109099c33a97f262e8d6db644d
Andrei <andrei.ungur@mail.mcgill.ca>
2019-03-07 16:58:13 -0500

    Add basic selenium setup

--- python-version/selenium-tests.py
+++ python-version/selenium-tests.py
@@ -1,1 +1,8 @@
-print("hello world")
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+opts = Options()
+opts.set_headless()
+assert opts.headless  # Operating in headless mode
+browser = Firefox(options=opts)
+browser.get('https://duckduckgo.com')
+
